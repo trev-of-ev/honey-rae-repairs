e6621dd5057ff92666f830a5e8e7eaf263b2c068
Trevor Guinn <tdguinn87@gmail.com>
2022-11-29 22:52:57 -0600

    Honey Rae Repair pt3

--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -1,7 +1,20 @@
-export const ApplicationViews = () => {
-	return <>
-		<h1 className="title--main">Honey Rae Repairs</h1>
-		<div>Your one-stop shop for repairing your tech</div>
-	</>
-}
+import { Outlet, Route, Routes } from "react-router-dom"
+import { TicketList } from "../tickets/TicketList"
 
+export const ApplicationViews = () => {
+	return (
+        <Routes>
+            <Route path="/" element={
+                <>
+                    <h1>Honey Rae Repair Shop</h1>
+                    <div>Your one-stop-shop to get all your electronics fixed</div>
+
+                    <Outlet />
+                </>
+            }>
+
+                <Route path="tickets" element={ <TicketList /> } />
+            </Route>
+        </Routes>
+    )
+}
